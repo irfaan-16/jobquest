1899e59baeb7565ae9381c9d4ab8157aa2239113
Irfaan <irfaank778@gmail.com>
2024-08-29 18:23:23 +0530

    changed the design of home and navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,5 @@
+import { Search } from "lucide-react";
+
 const Home = () => {
   return (
     <div className="text-center flex flex-col gap-16 justify-center p-14">
@@ -15,13 +17,24 @@ const Home = () => {
           -powered.
         </h1>
 
-        <div className="flex items-center gap-4 justify-center mt-10">
-          <button className="rounded-full bg-green-600 text-white px-4 py-2 fpnt">
+        <div className="flex items-center gap-4 justify-center mt-14">
+          <div className="flex items-center">
+            <input
+              placeholder="search for a job"
+              type="text"
+              className="font-bold rounded-md p-2 px-4 cursor-pointer backdrop:blur-2xl outline-none bg-[#ffffff9b]  placeholder:text-gray-600 pr-10 max-w-60 border-2 border-green-500"
+            />
+            <Search className="-ml-8 pointer-events-none" />
+          </div>
+          <button className="bg-green-500 text-white rounded-md p-2 cursor-pointer font-bold drop-shadow-highlight hover:bg-transparent hover:text-green-500 transition hover:drop-shadow-none hover:border-green-500 border-2 border-transparent duration-[250]">
+            search
+          </button>
+          {/* <button className="rounded-full bg-green-600 text-white px-4 py-2 fpnt">
             get started →
           </button>
           <button className="rounded-full px-4 py-2 text-black/85 font-bold border-green-500 border-2">
             explore jobs
-          </button>
+          </button> */}
         </div>
       </div>
 
@@ -30,76 +43,88 @@ const Home = () => {
           Discover jobs perfectly matched to you
         </h1>
 
-        <div className="grid ">
-          <div className="rounded-lg backdrop:blur-xl w-[35rem] text-center ">
-            <h3 className="text-3xl font-extrabold mb-8 underlined">
-              🔥 The <span className="text-green-600">Difference</span> We Make
-              →
-            </h3>
-            <p className="w-96 font-bold animate-float text-center m-auto">
-              Our AI analyzes your skills, preferences, and experience to
-              connect you with job opportunities that align with your career
-              goals.
-            </p>
+        <div className="flex justify-between mt-20 min-h-[400px] ">
+          <div className="flex flex-col justify-between">
+            <div className="rounded-lg backdrop:blur-xl w-[35rem] text-center">
+              <h3 className="text-3xl font-extrabold mb-8 underlined">
+                🔥 The <span className="text-green-600">Difference</span> We
+                Make →
+              </h3>
+              <ul>
+                <li className=" font-bold animate-float text-center m-auto before:content-['🚀'] before:mr-2">
+                  Analyze your skills and experience with advanced AI.
+                </li>
+
+                <li className=" font-bold animate-float text-center m-auto before:content-['🚀'] before:mr-2">
+                  Tailor job opportunities to match your preferences.
+                </li>
+
+                <li className=" font-bold animate-float text-center m-auto before:content-['🚀'] before:mr-2">
+                  Connect with roles aligned with your career goals.
+                </li>
+              </ul>
+            </div>
+
+            <div className="rounded-lg backdrop:blur-xl w-[35rem] text-center ml-16">
+              <h3 className="text-3xl font-extrabold mb-8 underlined">
+                🔥 How Its<span className="text-green-600"> Done</span>→
+              </h3>
+              <ul>
+                <li className=" font-bold animate-float text-center m-auto before:content-['🚀'] before:mr-2">
+                  Upload your resume or connect your LinkedIn.
+                </li>
+
+                <li className=" font-bold animate-float text-center m-auto before:content-['🚀'] before:mr-2">
+                  Set your job preferences.
+                </li>
+
+                <li className=" font-bold animate-float text-center m-auto before:content-['🚀'] before:mr-2">
+                  Receive personalized job recommendations.
+                </li>
+              </ul>
+            </div>
           </div>
 
-          <div className="rounded-lg backdrop:blur-xl w-[35rem] text-center ">
-            <h3 className="text-3xl font-extrabold mb-8 underlined">
-              🔥 How Its<span className="text-green-600"> Done</span>→
-            </h3>
-            <ul>
-              <li className="w-96 font-bold animate-float text-center m-auto before:content-['🚀'] before:mr-2">
-                Upload your resume or connect your LinkedIn.
-              </li>
+          <div className="flex flex-col justify-between">
+            <div className="rounded-lg backdrop:blur-xl w-[35rem] text-center">
+              <h3 className="text-3xl font-extrabold mb-8 underlined">
+                🔥 Explore
+                <span className="text-green-600"> Opportunitiess</span>→
+              </h3>
+              <ul>
+                <li className=" font-bold animate-float text-center m-auto before:content-['🚀'] before:mr-2">
+                  Access a diverse range of job listings.
+                </li>
 
-              <li className="w-96 font-bold animate-float text-center m-auto before:content-['🚀'] before:mr-2">
-                Set your job preferences.
-              </li>
+                <li className=" font-bold animate-float text-center m-auto before:content-['🚀'] before:mr-2">
+                  Find roles that align with your interests.
+                </li>
 
-              <li className="w-96 font-bold animate-float text-center m-auto before:content-['🚀'] before:mr-2">
-                Receive personalized job recommendations.
-              </li>
-            </ul>
-          </div>
+                <li className=" font-bold animate-float text-center m-auto before:content-['🚀'] before:mr-2">
+                  Get personalized insights to guide your career path.
+                </li>
+              </ul>
+            </div>
 
-          <div className="rounded-lg backdrop:blur-xl w-[35rem] text-center ">
-            <h3 className="text-3xl font-extrabold mb-8 underlined">
-              🔥 Explore
-              <span className="text-green-600"> Opportunitiess</span>→
-            </h3>
-            <ul>
-              <li className="w-96 font-bold animate-float text-center m-auto before:content-['🚀'] before:mr-2">
-                Access a diverse range of job listings.
-              </li>
+            <div className="rounded-lg backdrop:blur-xl w-[35rem] text-center -ml-28">
+              <h3 className="text-3xl font-extrabold mb-8 underlined">
+                🔥 Your
+                <span className="text-green-600"> Job</span> Adventure →
+              </h3>
+              <ul>
+                <li className=" font-bold animate-float text-center m-auto before:content-['🚀'] before:mr-2">
+                  Embark on a journey to discover unique career paths.
+                </li>
 
-              <li className="w-96 font-bold animate-float text-center m-auto before:content-['🚀'] before:mr-2">
-                Find roles that align with your interests.
-              </li>
+                <li className=" font-bold animate-float text-center m-auto before:content-['🚀'] before:mr-2">
+                  Engage with interactive, personalized job exploration.
+                </li>
 
-              <li className="w-96 font-bold animate-float text-center m-auto before:content-['🚀'] before:mr-2">
-                Get personalized insights to guide your career path.
-              </li>
-            </ul>
-          </div>
-
-          <div className="rounded-lg backdrop:blur-xl w-[35rem] text-center ">
-            <h3 className="text-3xl font-extrabold mb-8 underlined">
-              🔥 Your
-              <span className="text-green-600"> Job</span> Adventure →
-            </h3>
-            <ul>
-              <li className="w-96 font-bold animate-float text-center m-auto before:content-['🚀'] before:mr-2">
-                Embark on a journey to discover unique career paths.
-              </li>
-
-              <li className="w-96 font-bold animate-float text-center m-auto before:content-['🚀'] before:mr-2">
-                Engage with interactive, personalized job exploration.
-              </li>
-
-              <li className="w-96 font-bold animate-float text-center m-auto before:content-['🚀'] before:mr-2">
-                Unlock new opportunities with innovative matching technology.
-              </li>
-            </ul>
+                <li className=" font-bold animate-float text-center m-auto before:content-['🚀'] before:mr-2">
+                  Unlock new opportunities with innovative matching technology.
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,33 @@
+import { useState,useEffect } from "react";
+
 const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) { // Change this value as needed
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+
   return (
-    <nav className="border-b-2 border-gray-200 p-6 flex items-center justify-between">
-      <h1 className="text-green-500 font-bold text-xl cursor-pointer tracking-wider">
+    <nav className={`z-10 backdrop:blur-lg border-b-2 border-gray-200 p-6 flex items-center justify-between ${isScrolled&&'bg-green-600 !text-white drop-shadow-2xl'} sticky top-0 transition`}>
+      <h1 className={`${isScrolled?'text-white':'text-green-500'} font-bold text-xl cursor-pointer tracking-wider`}>
         jobquest
       </h1>
       <div>
-        <ul className="flex gap-12 text-gray-400 items-center font-bold">
+        <ul className="flex gap-12  items-center font-bold">
           <li className="cursor-pointer">Features</li>
           <li className="cursor-pointer">Jobs</li>
           <li className="cursor-pointer">FAQ</li>
@@ -13,7 +35,7 @@ const Navbar = () => {
       </div>
 
       <div className="flex gap-4 items-center">
-        <a href="/login" className="text-gray-400">
+        <a href="/login">
           Login
         </a>
         <a
